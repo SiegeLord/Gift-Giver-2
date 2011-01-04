ec24f6f3b4f465ce1914ebd70a0100e61bb229df
SiegeLord <slabode@aim.com>
2011-01-03 21:41:14 -0500

    Re-enable optimizations. They seem to work fine now.

--- linux_build.sh
+++ linux_build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-ldc main.d giftgiver2/*.d -oq -of="gg2" -L-lallegro_ttf -L-lallegro_font -L-lallegro_image -L-lallegro_primitives -L-lallegro_audio  -L-lallegro_acodec  -L-lallegro -L-ldallegro5
+ldc main.d giftgiver2/*.d -oq -O5 -of="gg2" -L-lallegro_ttf -L-lallegro_font -L-lallegro_image -L-lallegro_primitives -L-lallegro_audio  -L-lallegro_acodec  -L-lallegro -L-ldallegro5
 rm *.o
